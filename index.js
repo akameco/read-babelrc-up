6e73072f587be3766b1f1488f3f8193e9aa2f819
akameco <akameco.t@gmail.com>
2017-05-17 09:48:24 +0900

    Support package.json and .babelrc.js

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
-'use strict';
-const findUp = require('find-up');
-const loadJsonFile = require('load-json-file');
+'use strict'
+const path = require('path')
+const fs = require('fs')
+const findUp = require('find-up')
+const json5 = require('json5')
+
+const BABELRC = '.babelrc'
+const BABELRC_JS = '.babelrc.js'
+const PKG = 'package.json'
+
+const readConfigJs = fp => {
+  try {
+    const configModule = require(fp)
+    return configModule && configModule.__esModule
+      ? configModule.default || undefined
+      : configModule
+  } catch (err) {
+    err.message = `${fp}: Error while loading config = ${err.message}`
+    throw err
+  }
+}
+
+const loadConfig = fp => {
+  const file = path.basename(fp)
+  const obj = fs.readFileSync(fp, 'utf-8')
+
+  switch (file) {
+    case BABELRC_JS:
+      return readConfigJs(fp)
+    case BABELRC:
+      return json5.parse(obj)
+    case PKG:
+      return JSON.parse(obj).babel
+    default:
+      return {}
+  }
+}
 
 module.exports = opts => {
-	return findUp('.babelrc', opts).then(fp => {
-		if (!fp) {
-			return {};
-		}
+  return findUp([BABELRC, BABELRC_JS, PKG], opts).then(fp => {
+    if (!fp) {
+      return {}
+    }
 
-		return loadJsonFile(fp).then(babel => ({babel, path: fp}));
-	});
-};
+    const babel = loadConfig(fp)
+
+    return { babel, path: fp }
+  })
+}
 
 module.exports.sync = opts => {
-	const fp = findUp.sync('.babelrc', opts);
+  const fp = findUp.sync([BABELRC, BABELRC_JS, PKG], opts)
 
-	if (!fp) {
-		return {};
-	}
+  if (!fp) {
+    return {}
+  }
 
-	return {
-		babel: loadJsonFile.sync(fp, opts),
-		path: fp
-	};
-};
+  const babel = loadConfig(fp)
+
+  return { babel, path: fp }
+}
--- test.js
+++ test.js
@@ -1,20 +1,57 @@
-import path from 'path';
-import test from 'ava';
-import fn from '.';
+import path from 'path'
+import test from 'ava'
+import fn from '.'
 
-const cwd = path.join('fixture', 'app');
-const babelrcPath = path.resolve('fixture', '.babelrc');
+const babelrcPath = path.resolve('fixture', 'babelrc', '.babelrc')
+const babelrcJsPath = path.resolve('fixture', 'babelrcjs', '.babelrc.js')
+const pkgPath = path.resolve('fixture', 'pkg', 'package.json')
 
 test('async', async t => {
-	const x = await fn({cwd});
-	t.is(x.path, babelrcPath);
-	t.deepEqual(x.babel.presets, ['es2015', 'react']);
-	t.deepEqual(x.babel.plugins, ['transform-class-properties']);
-});
+  const cwd = path.join('fixture', 'babelrc', 'app')
+  const x = await fn({ cwd })
+  t.is(x.path, babelrcPath)
+  t.deepEqual(x.babel.presets, ['es2015', 'react'])
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
 
 test('sync', t => {
-	const x = fn.sync({cwd});
-	t.is(x.path, babelrcPath);
-	t.deepEqual(x.babel.presets, ['es2015', 'react']);
-	t.deepEqual(x.babel.plugins, ['transform-class-properties']);
-});
+  const cwd = path.join('fixture', 'babelrc', 'app')
+  const x = fn.sync({ cwd })
+  t.is(x.path, babelrcPath)
+  t.deepEqual(x.babel.presets, ['es2015', 'react'])
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
+
+test('parse package.json', async t => {
+  const cwd = path.join('fixture', 'pkg')
+  const x = await fn({ cwd })
+  t.is(x.path, pkgPath)
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
+
+test('parse package.json - sync', t => {
+  const cwd = path.join('fixture', 'pkg')
+  const x = fn.sync({ cwd })
+  t.is(x.path, pkgPath)
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
+
+test('parse babelrc.js', async t => {
+  const cwd = path.join('fixture', 'babelrcjs')
+  const x = await fn({ cwd })
+  t.is(x.path, babelrcJsPath)
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
+
+test('parse babelrc.js - sync', t => {
+  const cwd = path.join('fixture', 'babelrcjs')
+  const x = fn.sync({ cwd })
+  t.is(x.path, babelrcJsPath)
+  t.deepEqual(x.babel.plugins, ['transform-class-properties'])
+  t.snapshot(x)
+})
